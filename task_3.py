27da850133d1a2c84c90be25cfbc59aac4018483
ivan <ivan.pavlov.alk@gmail.com>
2021-02-18 21:28:28 +0200

    Task 3 - Complited

---
+++ task_3.py
@@ -0,0 +1,14 @@
+percent1 = [0, 5, 6, 7, 8, 9]
+percent2 = [2, 3, 4]
+
+quest = input("Введите число: ")
+
+if int(quest) > 9 and int(quest[0]) == 1:
+    print(quest, "процентов")
+elif int(quest) > 9 and int(quest[1]) in percent1:
+    print(quest, "процентов")
+elif int(quest) == 1 or int(quest[1]) == 1:
+    print(quest, "процент")
+elif int(quest[0]) or int(quest[1]) in percent2:
+    print(quest, "процента")
+
